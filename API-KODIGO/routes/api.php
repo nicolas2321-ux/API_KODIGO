7913ae3bd705755fa9a62cc277870e94b65908bb
nicolas2321-ux <00035819@uca.edu.sv>
2022-10-09 11:10:14 -0600

    add files

--- API-KODIGO/routes/api.php
+++ API-KODIGO/routes/api.php
@@ -21,28 +21,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 require __DIR__ . '/json-api-auth.php';
 
-/*
-|--------------------------------------------------------------------------
-| An example of how to use the verified email feature with api endpoints
-|--------------------------------------------------------------------------
-|
-| Here examples of a route using Sanctum middleware and verified middleware.
-| And another route using Passport middleware and verified middleware.
-| You can install and use one of this official packages.
-|
-*/
-
-//Route::get('/verified-middleware-example', function () {
-//    return response()->json([
-//        'message' => 'the email account is already confirmed now you are able to see this message...',
-//    ]);
-//})->middleware('auth:sanctum', 'verified');
-
-//Route::get('/verified-middleware-example', function () {
-//    return response()->json([
-//        'message' => 'the email account is already confirmed now you are able to see this message...',
-//    ]);
-//})->middleware('auth:api', 'verified');
 Route::get('/articulos',[ArticuloController::class,'index']);
 Route::post('/articulos',[ArticuloController::class,'store']);
 Route::put('/articulos/{id}',[ArticuloController::class,'update']);
